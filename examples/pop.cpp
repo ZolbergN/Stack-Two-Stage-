7699b1b807fea1a978255ad72af699a8bc831cf1
Roman <exculibur@rambler.ru>
2017-10-03 00:17:21 +0300

    Update pop.cpp

--- examples/pop.cpp
+++ examples/pop.cpp
@@ -1,5 +1,18 @@
+#include <iostream>
 #include "stack.hpp"
 
+using namespace std;
+
+void stack_pop() {
+	Stack<int> st;
+	for(int i = 0; i < 10; i++) { 
+		st.push(i); 
+	}
+	for(int j = 0; j < 3; j++) {
+		cout << st.pop() << " "; 
+	}
+}
+
 int main() {
-	stack_pop(3);
+	stack_pop();
 }
