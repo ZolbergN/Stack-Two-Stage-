82ed699ee121bc12fb1b903d2a4b068e5b8df9cf
Roman <exculibur@rambler.ru>
2017-10-03 00:11:35 +0300

    Update init.cpp

--- examples/init.cpp
+++ examples/init.cpp
@@ -1,5 +1,13 @@
+#include <iostream>
 #include "stack.hpp"
 
+using namespace std;
+
+void stackInit() {
+	Stack<int> st;
+	cout << "Created st" << endl;
+}
+
 int main() {
-	stack_init();
+	stackInit();
 }
