f338d39ee1b41cdc693ab589f5791662692dfbcb
Roman <exculibur@rambler.ru>
2017-10-02 23:59:07 +0300

    Update empty.cpp

--- examples/empty.cpp
+++ examples/empty.cpp
@@ -1,5 +1,19 @@
+#include <iostream>
 #include "stack.hpp"
 
+using namespace std;
+
+void stack_empty() {
+	cout << "empty" << endl;
+	Stack<char> arr;
+	char arr1[] = "JOKA";
+	for(int i = 0; i < strlen(arr1); i++) { arr.push(arr1[i]); }
+	while(!arr.empty()) {
+		cout << arr.pop() << " ";
+	}
+	cout < endl;
+}
+
 int main() {
-	stack_empty(4);
+	stack_empty();
 }
