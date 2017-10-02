d2b3331b52ec93db2165e088d46184c5a2c2e075
Roman <exculibur@rambler.ru>
2017-10-03 00:19:21 +0300

    Update push.cpp

--- examples/push.cpp
+++ examples/push.cpp
@@ -1,5 +1,18 @@
+#include <iostream>
 #include "stack.hpp"
 
+using namespace std;
+
+void stack_push() {
+	Stack<int> st;
+	for(int i = 0; i < 10; i++) {
+		arrayInt.push(i*2); 
+	}
+	for(size_t j = 0; j < arrayInt.count(); j++) {
+		cout << arrayInt[j] << " "; 
+	}
+}
+
 int main() {
-	stack_push(4);
+	stack_push();
 }
