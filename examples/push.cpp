3688d459666cc42aed2172b0433073583fff412f
Roman <exculibur@rambler.ru>
2017-10-05 14:27:57 +0300

    Update push.cpp

--- examples/push.cpp
+++ examples/push.cpp
@@ -6,10 +6,10 @@ using namespace std;
 void stack_push() {
 	stack<int> st;
 	for(int i = 0; i < 10; i++) {
-		arrayInt.push(i*2); 
+		st.push(i*2); 
 	}
-	for(size_t j = 0; j < arrayInt.count(); j++) {
-		cout << arrayInt[j] << " "; 
+	for(size_t j = 0; j < st.count(); j++) {
+		cout << st[j] << " "; 
 	}
 }
 
